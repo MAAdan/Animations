48ac1a8708b5410ae48658ae9d71b05e0e9e7967
MAAdan <adan.miguel@gmail.com>
2018-05-30 23:19:50 +0200

    Changed the font size and type when the menu is toggled

--- Animations/ViewController.swift
+++ Animations/ViewController.swift
@@ -20,6 +20,7 @@ class ViewController: UIViewController {
     
     let menuHeightLimits: Limits = (min: 70.0, max: 200.0)
     let titleXPositionLimits: Limits = (min: -120.0, max: 0.0)
+    let titleFontSizeLimits: Limits = (min: 19.0, max: 27.0)
     
     var isMenuOpen = false
     
@@ -32,12 +33,32 @@ class ViewController: UIViewController {
     @IBAction func actionToggleMenu(_ sender: UIButton) {
         isMenuOpen = !isMenuOpen
         titleLabel.text = isMenuOpen ? "Select item" : "Simple list"
+        titleLabel.font = isMenuOpen ? UIFont.boldSystemFont(ofSize: titleFontSizeLimits.max) : UIFont.systemFont(ofSize: titleFontSizeLimits.min)
         self.view.layoutIfNeeded()
         
+        let constraints = titleLabel.superview?.constraints.filter{ $0.identifier == "TitleCenterXConstraint" || $0.identifier ==  "TitleCenterYConstraint"}
+        
         // Modifying the constraints by identifier
-        let constraints = titleLabel.superview?.constraints.filter{ $0.identifier == "TitleCenterXConstraint" }
-        if let titleCenterXConstraint = constraints?.first {
+        if let titleCenterXConstraint = constraints?.filter({ $0.identifier == "TitleCenterXConstraint"}).first {
             titleCenterXConstraint.constant = isMenuOpen ? titleXPositionLimits.min : titleXPositionLimits.max
+        }
+        
+        // Animating by replacing the affected constraint
+        if let titleCenterYConstraint = constraints?.filter({ $0.identifier == "TitleCenterYConstraint"}).first {
+            titleCenterYConstraint.isActive = false
+            
+            let newConstraint = NSLayoutConstraint(
+                item: titleLabel,
+                attribute: .centerY,
+                relatedBy: .equal,
+                toItem: titleLabel.superview,
+                attribute: .centerY,
+                multiplier: isMenuOpen ? 0.60 : 1.0,
+                constant: 5.0
+            )
+            
+            newConstraint.identifier = "TitleCenterYConstraint"
+            newConstraint.isActive = true
         }
         
         //Modifying the constraints by IBOutlet
